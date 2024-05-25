5da5ca5271ce4c6ce120b7f2647e4accee8afdb8
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:38 +0530

    Add src/commands/manageRoles.js

---
+++ src/commands/manageRoles.js
@@ -0,0 +1,10 @@
+// Manage Roles Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'roles',
+  description: 'Manage roles in the server',
+  execute(message, args) {
+    // Logic to manage roles
+  }
+}
