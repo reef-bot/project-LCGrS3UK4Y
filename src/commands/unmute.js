05322ac5dcd12849dd8e21f42a7f99d20ffb8290
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:37 +0530

    Add src/commands/unmute.js

---
+++ src/commands/unmute.js
@@ -0,0 +1,10 @@
+// Unmute Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'unmute',
+  description: 'Unmute a user in the server',
+  execute(message, args) {
+    // Logic to unmute user
+  }
+}
