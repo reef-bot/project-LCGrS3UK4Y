21272ab9de247de72b27e135d9387255c9b544a3
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:36 +0530

    Add src/commands/kick.js

---
+++ src/commands/kick.js
@@ -0,0 +1,10 @@
+// Kick Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'kick',
+  description: 'Kick a user from the server',
+  execute(message, args) {
+    // Logic to kick user
+  }
+}
