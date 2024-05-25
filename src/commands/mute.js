880dc8ab1fc5d6852ab92d5c5e4158a53a919536
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:37 +0530

    Add src/commands/mute.js

---
+++ src/commands/mute.js
@@ -0,0 +1,10 @@
+// Mute Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'mute',
+  description: 'Mute a user in the server',
+  execute(message, args) {
+    // Logic to mute user
+  }
+}
