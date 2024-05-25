22d7e5d9b3ff9f4a307f231a16ecdcddf3ec5f96
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:36 +0530

    Add src/commands/ban.js

---
+++ src/commands/ban.js
@@ -0,0 +1,10 @@
+// Ban Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'ban',
+  description: 'Ban a user from the server',
+  execute(message, args) {
+    // Logic to ban user
+  }
+}
