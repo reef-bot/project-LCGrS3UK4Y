622e409c7d53ffa4d9565e3f9ef6ecc5c98ac13e
DRIX10's alternative account <126459477+reef-bot@users.noreply.github.com>
2024-05-25 09:43:38 +0530

    Add src/commands/deleteMessage.js

---
+++ src/commands/deleteMessage.js
@@ -0,0 +1,10 @@
+// Delete Message Command
+const { Client, Message } = require('discord.js');
+
+module.exports = {
+  name: 'delete',
+  description: 'Delete a message in the server',
+  execute(message, args) {
+    // Logic to delete message
+  }
+}
